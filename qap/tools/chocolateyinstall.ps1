26d411b94039fbacff8452ab871749c3c123ba86
Nicola Farina <nicola@nicolafarina.it>
2017-10-03 23:57:23 +0200

    v. 8.5.3
    QAP 8.5.3

---
+++ qap/tools/chocolateyinstall.ps1
@@ -0,0 +1,44 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://www.quickaccesspopup.com/download/quickaccesspopup-setup.exe'
+$url64      = $url
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'Quick Access Popup'
+
+  checksum      = 'FA4A52175C170EF3D3AB14E0CE05ECB4'
+  checksumType  = 'md5'
+  checksum64    = 'FA4A52175C170EF3D3AB14E0CE05ECB4'
+  checksumType64= 'md5'
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
