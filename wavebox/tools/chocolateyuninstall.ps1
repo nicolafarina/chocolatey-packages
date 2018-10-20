d783079ec8e8cd53988e28cd906bf0ccf7dbb622
Nicola Farina <nicola@nicolafarina.it>
2018-10-20 18:21:59 +0200

    Fix

---
+++ wavebox/tools/chocolateyuninstall.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = '*Wavebox*'
+  fileType      = 'EXE'
+  silentArgs    = "--uninstall -s"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % { 
+    $packageArgs['file'] = $env:USERPROFILE + "\AppData\Local\wavebox\Update.exe" 
+    if ($packageArgs['fileType'] -eq 'MSI') {
+      $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+      $packageArgs['file'] = ''
+    }
+
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
