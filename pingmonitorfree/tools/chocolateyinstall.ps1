ee65f155957578f1750de34b9cf4034503f3dd94
Nicola Farina <nicola@nicolafarina.it>
2018-10-20 11:50:16 +0200

    Ping Monitor Free new package

---
+++ pingmonitorfree/tools/chocolateyinstall.ps1
@@ -0,0 +1,44 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://storage.emcosoftware.com/download/pingmonitor/PingMonitorFreeSetup.exe'
+$url64      = $url
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'EMCO Ping Monitor Free'
+
+  checksum      = 'bda07d6f16cdd466aa2db3dcefd7a3d1'
+  checksumType  = 'md5'
+  checksum64    = 'bda07d6f16cdd466aa2db3dcefd7a3d1'
+  checksumType64= 'md5'
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/s'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
