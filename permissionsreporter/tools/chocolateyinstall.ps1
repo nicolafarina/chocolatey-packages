e3aed68198e07bcfdac16aaef81c08f99125fe75
Nicola Farina <nicola@nicolafarina.it>
2018-10-21 10:51:55 +0200

    New package

---
+++ permissionsreporter/tools/chocolateyinstall.ps1
@@ -0,0 +1,44 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.comfort-software.com/download/cc_setup.exe'
+$url64      = $url
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'Comfort Clipboard Pro'
+
+  checksum      = '3c5636051025dfc9f5e2540699da5f9c'
+  checksumType  = 'md5'
+  checksum64    = '3c5636051025dfc9f5e2540699da5f9c'
+  checksumType64= 'md5'
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
