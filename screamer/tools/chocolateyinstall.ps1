2588c6c47db701ee32a1ad6f4060027fcf9c1930
Nicola Farina <nicola@nicolafarina.it>
2018-11-01 09:19:01 +0100

    Screamer Radio package added

---
+++ screamer/tools/chocolateyinstall.ps1
@@ -0,0 +1,44 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://apps.steamcore.se/Screamer/Releases/ScreamerSetup.exe'
+$url64      = $url
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'Screamer Radio'
+
+  checksum      = '5e3cb4a6f62a1e124c64e0bf52b8b554'
+  checksumType  = 'md5'
+  checksum64    = '5e3cb4a6f62a1e124c64e0bf52b8b554'
+  checksumType64= 'md5'
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
